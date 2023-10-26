2234e87bfd1baf5f232b85613a4d0fcdb43d491b
sotakari <146611649+sotakari@users.noreply.github.com>
2023-10-26 13:55:56 +0000

    [Finish] 1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,13 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  resources :books, only: [:new, :create, :index, :show]
-  resources :users, only: [:show]
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :index, :update]
+  get "homes/about" => "homes#about", as: "about"
 end
